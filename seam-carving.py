e12ff191861faf4817d7e74d11ff92a0d2df4755
David Robinson <dvsrobinson@gmail.com>
2019-04-07 03:57:15 +1200

    Implement seam carving demo in python

--- seam-carving.py
+++ seam-carving.py
@@ -63,23 +63,16 @@ def carveSeam(img, energy_cumulative, backtrack):
     
     return seam_img, trimmed_img  
     
-
 def seamCarve(input_img, axis=1, visualize=False):
-
     energy_map = computeEnergy(input_img)
-
     if axis == 0: # Trim rows
         raise NotImplementedError
-
     elif axis == 1: # Trim cols
         energy_cumulative, backtrack = minSeam(input_img, energy_map)
         _, trimmed_img = carveSeam(input_img, energy_cumulative, backtrack)
-
     else:
         raise ValueError('Invalid axis value')
-
     return trimmed_img
-
 
 def main():
     ap = argparse.ArgumentParser()
@@ -92,22 +85,14 @@ def main():
     args = vars(ap.parse_args())
     
     img = cv.imread(args['image'], cv.IMREAD_COLOR)
-    cv.imshow('image', img)
-
+    cv.imshow('input', img)
 
     trim_width = img.shape[1] - int(img.shape[1] * float(args['width']))
     for i in tqdm(range(trim_width)):
         img = seamCarve(img, axis=1, visualize=False)
 
-    # trim_height = img.shape[0] - int(img.shape[0] * float(args['height']))
-    # for i in range(trim_height):
-    #     img = seamCarve(img, axis=0, visualize=False)
-
     # Display result
-    cv.imshow('image', img)
-    # cv.imshow('energy', energy_map / energy_map.max())
-    # cv.imshow('seam', seam_img)
-    # cv.imshow('output', output_img)
+    cv.imshow('output', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
