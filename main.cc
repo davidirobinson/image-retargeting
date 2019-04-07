7ea255a8cd6f39225628c7827467318346b10767
David Robinson <dvsrobinson@gmail.com>
2019-04-07 13:16:20 +1200

    Add iterators to get min cost seam

--- main.cc
+++ main.cc
@@ -4,8 +4,10 @@
 //
 
 #include <string>
+#include <vector>
 #include <unistd.h>
 #include <iostream>
+#include <algorithm>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
@@ -34,11 +36,63 @@ cv::Mat computeEnergy(const cv::Mat &input)
     return energy;
 }
 
+void minSeam( 
+    const cv::Mat &energy, 
+    cv::Mat &energy_cumulative,
+    cv::Mat &backtrack)
+{
+    energy_cumulative = energy.clone();
+    backtrack = cv::Mat::zeros(energy.size(), CV_8U);
+
+    // Create iterator for argmin computations
+    cv::MatConstIterator_<uchar> prev_row_iter = energy_cumulative.begin<uchar>();
+    
+    // Begin with second row
+    for (size_t row = 1; row < energy.rows; ++row)
+    {
+        uchar* energy_cumulative_ptr = energy_cumulative.ptr<uchar>(row);
+        uchar* backtrack_ptr = energy_cumulative.ptr<uchar>(row);
+
+        std::cout << "\n\n\n\n\nBegin\n\n\n\n\n" << std::endl;
+        for (size_t col = 0; col < energy.cols; ++col)
+        {
+            // Deal with first and last column
+            size_t prev_col_range = (col == 0 ? 0 : 1);
+            size_t post_col_range = (col == energy.cols-1 ? 0 : 1);
+
+            // Get idx of min energy from previous row, on 8-connected cols  
+            const auto result = std::min_element(
+                prev_row_iter - prev_col_range - 1, prev_row_iter + post_col_range);
+            const auto idx = std::distance(prev_row_iter - prev_col_range - 1, result);
+
+            std::cout << "idx: " << idx << " result " << int(*result);
+            std::cout 
+                << "\nbefore: " << int(*(prev_row_iter - prev_col_range)) 
+                << " curr: " << int(*prev_row_iter) 
+                << " ahead: " << int(*(prev_row_iter + post_col_range)) << std::endl;
+ 
+            for (auto it = prev_row_iter - prev_col_range - 1; it != prev_row_iter + post_col_range; ++it)
+            {
+                std::cout << int(*it) << " ";
+            }
+            std::cout << " test " << std::endl;
+
+            backtrack_ptr[col] = idx + col - prev_col_range;
+            energy_cumulative_ptr[col] += *result;
+
+            prev_row_iter++;
+        }
+    }
+}
+
 void seamCarveCol(cv::Mat &input)
 {
     const auto energy = computeEnergy(input);
     cv::imshow("grad", energy);
     cv::waitKey(0);
+
+    cv::Mat energy_cumulative, backtrack;
+    minSeam(energy, energy_cumulative, backtrack);
 }
 
 cv::Mat retargetImg(const cv::Mat &input, const int rows, const int cols)
@@ -104,17 +158,17 @@ int main(int argc, char *argv[])
 
     cv::Mat retargeted_img = retargetImg(img, target_rows, target_cols);
     
-    // cv::namedWindow("img", cv::WINDOW_NORMAL);
-    cv::imshow("img", img);
-    cv::imshow("retargeted", retargeted_img);
-    cv::waitKey(0);
- 
+    // // cv::namedWindow("img", cv::WINDOW_NORMAL);
+    // cv::imshow("img", img);
+    // cv::imshow("retargeted", retargeted_img);
+    // cv::waitKey(0);
+
     // while (cv::waitKey(30) != 'q')
     // {    
     //     std::cout << "loop" << std::endl;
     //     img -= cv::Scalar(1,1,1);
     //     cv::imshow("img", img);
     // }
-    cv::destroyWindow("img");
+    // cv::destroyWindow("img");
     return 0;
 }
